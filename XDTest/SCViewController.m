e5b9b9152ac1d3d6f121f348107d09935d2064dc
Morgan Collino <morgan.collino@gmail.com>
2014-04-15 21:00:20 -0700

    Extension UIButton + UIImageView

--- XDTest/SCViewController.m
+++ XDTest/SCViewController.m
@@ -7,23 +7,54 @@
 //
 
 #import "SCViewController.h"
+#import "UIImageView_XDShape.h"
+#import "UIButton_XDShape.h"
 
 @interface SCViewController ()
+@property (nonatomic, retain) IBOutlet UIImageView *imageViewRound;
+@property (nonatomic, retain) IBOutlet UIImageView *imageViewRoundWithBorder;
 
+@property (nonatomic, retain) IBOutlet UIImageView *imageViewSquare;
+@property (nonatomic, retain) IBOutlet UIImageView *imageViewSquareWithBorder;
+@property (nonatomic, retain) IBOutlet UIButton *buttonRound;
+@property (nonatomic, retain) IBOutlet UIButton *buttonSquare;
 @end
 
 @implementation SCViewController
 
+@synthesize imageViewRound, imageViewSquare, buttonRound, buttonSquare, imageViewRoundWithBorder, imageViewSquareWithBorder;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    UIImage* image = [UIImage imageNamed: @"default.png"];
+    
+    // ImagesViews
+    imageViewRound.image = image;
+    [imageViewRound toRoundImageView];
+    imageViewSquare.image = image;
+    [imageViewSquare toSquareImageView];
+    
+    imageViewRoundWithBorder.image = image;
+    [imageViewRoundWithBorder toRoundImageViewWithBorderSize: 1 color: [UIColor redColor]];
+    imageViewSquareWithBorder.image = image;
+    [imageViewSquareWithBorder toSquareImageViewWithBorderSize:2  color: [UIColor blackColor]];
+
+    // Buttons
+    [buttonRound setBackgroundImage: image forState:UIControlStateNormal];
+    [buttonRound toRoundImageViewWithBorderSize: 1 color: [UIColor orangeColor]];
+    
+    [buttonSquare setBackgroundImage: image forState:UIControlStateNormal];
+    [buttonSquare toSquareImageViewWithBorderSize: 1 color: [UIColor redColor]];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    imageViewSquare = nil;
+    imageViewRound = nil;
 }
 
 @end
